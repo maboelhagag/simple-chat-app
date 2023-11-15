57168ad281cc33127dd1562b8ca02958ed63c6f5
maboelhagag <90697334+maboelhagag@users.noreply.github.com>
2023-11-16 00:10:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ function generateNickname() {
 }
 
 // Connect to the WebSocket server
-const socket = new WebSocket('https://maboelhagag.github.io/simple-chat-app/');
+const socket = new WebSocket('wss://maboelhagag.github.io/simple-chat-app/');
 
 // Handle incoming messages
 socket.onmessage = (event) => {
