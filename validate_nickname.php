a99fab8a71a12fd4cbac5096aca1ed6b81ddd54d
maboelhagag <90697334+maboelhagag@users.noreply.github.com>
2023-11-16 01:06:44 +0200

    Update validate_nickname.php

--- validate_nickname.php
+++ validate_nickname.php
@@ -1,53 +1,18 @@
 <?php
-$chatFile = "chat.txt";
-$userListFile = "user_list.txt";
 
-$action = $_GET["action"];
+$nickname = isset($_POST['nickname']) ? trim($_POST['nickname']) : '';
 
-if ($action === "check-nickname") {
-    $nickname = $_GET["nickname"];
-    $available = checkNicknameAvailability($nickname);
-    echo json_encode(["available" => $available]);
-} elseif ($action === "save-nickname") {
-    $nickname = $_GET["nickname"];
-    saveNickname($nickname);
-} elseif ($action === "send-message") {
-    $message = $_GET["message"];
-    saveMessage($message);
-} elseif ($action === "refresh-chat") {
-    echo getChatLog();
+// Validate the nickname (simplified check for demonstration purposes)
+if (isValidNickname($nickname)) {
+    // Append the nickname to chat.txt (simplified file appending for demonstration purposes)
+    file_put_contents('chat.txt', "$nickname," . $_SERVER['REMOTE_ADDR'] . PHP_EOL, FILE_APPEND);
+    echo json_encode(['success' => true]);
+} else {
+    echo json_encode(['success' => false, 'message' => 'Invalid nickname. Please choose another.']);
 }
 
-function checkNicknameAvailability($nickname) {
-    $log = file_get_contents($userListFile);
-    $logArray = explode("\n", $log);
-
-    foreach ($logArray as $entry) {
-        $entryParts = explode(",", $entry);
-        if (count($entryParts) >= 2) {
-            $savedNickname = $entryParts[0];
-            if ($savedNickname === $nickname) {
-                return false;
-            }
-        }
-    }
-
+function isValidNickname($nickname) {
+    // Check if the nickname is not in chat.txt (simplified check for demonstration purposes)
     return true;
-}
-
-function saveNickname($nickname) {
-    $ip = $_SERVER["REMOTE_ADDR"];
-    $logEntry = $nickname . "," . $ip . "\n";
-    file_put_contents($userListFile, $logEntry, FILE_APPEND);
-}
-
-function saveMessage($message) {
-    $nickname = $_GET["nickname"];
-    $logEntry = $nickname . ": " . $message . "<br>";
-    file_put_contents($chatFile, $logEntry, FILE_APPEND);
-}
-
-function getChatLog() {
-    return file_get_contents($chatFile);
 }
 ?>
