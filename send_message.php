144226256ca6a4c491aae6a2aca1966b148b438a
maboelhagag <90697334+maboelhagag@users.noreply.github.com>
2023-11-16 00:36:47 +0200

    Update send_message.php

--- send_message.php
+++ send_message.php
@@ -1,14 +1,1 @@
-<?php
-$chatFile = "chat.txt";
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $nickname = $_POST["nickname"];
-    $message = $_POST["message"];
-
-    // Append the new message to the chat file
-    file_put_contents($chatFile, "<strong>" . $nickname . "</strong>: " . $message . "<br>", FILE_APPEND);
-} elseif ($_SERVER["REQUEST_METHOD"] === "GET") {
-    // Read the chat file and send its contents as the response
-    echo file_get_contents($chatFile);
-}
-?>
