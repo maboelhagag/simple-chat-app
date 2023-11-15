92443b224e9399f6889b66efec3462fa38f34cc8
maboelhagag <90697334+maboelhagag@users.noreply.github.com>
2023-11-16 00:39:22 +0200

    Update private_chat.php

--- private_chat.php
+++ private_chat.php
@@ -1,1 +1,27 @@
+<?php
+// private_chat.php
 
+$targetUser = $_GET["targetUser"];
+
+if (empty($targetUser)) {
+    die("Invalid target user.");
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Private Chat</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div id="private-chat-container">
+        <h1>Private Chat with <?php echo $targetUser; ?></h1>
+        <div id="private-chat-messages"></div>
+        <input type="text" id="private-message-input" placeholder="Type your message...">
+        <button id="private-send-button">Send</button>
+    </div>
+
+    <script src="private_chat.js"></script>
+</body>
+</html>
