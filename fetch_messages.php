caab4d2fcbb9fdb9f89c8487f3dd70aaff332bef
maboelhagag <90697334+maboelhagag@users.noreply.github.com>
2023-11-16 00:36:29 +0200

    Update fetch_messages.php

--- fetch_messages.php
+++ fetch_messages.php
@@ -1,13 +1,1 @@
-<?php
 
-// For simplicity, messages are stored in a file
-// In a real-world scenario, you would use a database
-
-$messagesFile = 'messages.json';
-
-if (file_exists($messagesFile)) {
-    $messages = json_decode(file_get_contents($messagesFile), true);
-    echo json_encode(['messages' => $messages]);
-} else {
-    echo json_encode(['messages' => []]);
-}
