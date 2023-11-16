588f63ef0b311a013adadf51841d9c50b0e54b99
maboelhagag <90697334+maboelhagag@users.noreply.github.com>
2023-11-16 22:20:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-// Replace with your Firebase config
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCII9Jyj_l1yoq9ZliBbqMVeJRIRQDaRQw",
+    authDomain: "simple-chat-19662.firebaseapp.com",
+    databaseURL: "https://simple-chat-19662-default-rtdb.firebaseio.com",
+    projectId: "simple-chat-19662",
+    storageBucket: "simple-chat-19662.appspot.com",
+    messagingSenderId: "452631612399",
+    appId: "1:452631612399:web:514884f2f8c1c75c1e441a",
+    measurementId: "G-GYQY6WRY31"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
