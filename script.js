a258e51d2ec0cafe5dacee2aba66449ed3eee8d8
maboelhagag <90697334+maboelhagag@users.noreply.github.com>
2023-11-20 11:26:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,31 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+  const socket = io(); // Connect to the server
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAh75rx12k6LNfg5zQ4zJPzIifSCoNMQA4",
-  authDomain: "chatroom-73ec1.firebaseapp.com",
-  projectId: "chatroom-73ec1",
-  storageBucket: "chatroom-73ec1.appspot.com",
-  messagingSenderId: "525676968778",
-  appId: "1:525676968778:web:f5859440ae9844a8f61fb3",
-  measurementId: "G-GMYW7VKZYX"
-};
+  const chatMessages = document.getElementById('chat-messages');
+  const messageForm = document.getElementById('message-form');
+  const messageInput = document.getElementById('message-input');
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  // Add a new message to the chat interface
+  function addNewMessage(message) {
+    const newMessageElement = document.createElement('div');
+    newMessageElement.innerText = message;
+    chatMessages.appendChild(newMessageElement);
+  }
 
-const database = firebase.database();
-const chatBox = document.getElementById('chat-box');
-const messageForm = document.getElementById('message-form');
-const messageInput = document.getElementById('message-input');
-
-messageForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-    const message = messageInput.value.trim();
-
-    if (message !== '') {
-        sendMessage(message);
-    }
-
+  // Send a new message to the server
+  function sendMessage(e) {
+    e.preventDefault();
+    const message = messageInput.value;
+    socket.emit('chatMessage', message);
     messageInput.value = '';
-});
+  }
 
-function appendMessage(username, text) {
-    chatBox.innerHTML += `<p><strong>${username}:</strong> ${text}</p>`;
-    chatBox.scrollTop = chatBox.scrollHeight; // Scroll to the bottom
-}
+  // Receive new message from the server
+  socket.on('message', (message) => {
+    addNewMessage(message);
+  });
 
-function sendMessage(text) {
-    const username = prompt("Enter your username:");
-    const message = { username, text };
-
-    database.ref('messages').push(message);
-}
-
-// Listen for new messages in real-time
-database.ref('messages').on('child_added', (snapshot) => {
-    const message = snapshot.val();
-    appendMessage(message.username, message.text);
+  // Event listener for the message form submission
+  messageForm.addEventListener('submit', sendMessage);
 });
